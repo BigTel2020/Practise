dd920022a36164a062ca7854d30d7db33d830597
Terry <71273972+BigTel2020@users.noreply.github.com>
2020-10-26 20:18:24 +0000

    adding the images getting close to completion

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-
-
+var prev = document.getElementById("prev");
+var next = document.getElementById("next");
+var thumbnail = document.getElementsByClassName("thumbnail");
+var hero = document.getElementById("hero");
+var backgroungImg = new Array(
+    "images/bg1.png",
+    "images/bg2.png",
+    "images/bg3.png",
+    "images/bg4.png",
+    "images/bg5.png",
+);
