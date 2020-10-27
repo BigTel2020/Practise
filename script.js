ddc7b5a243fe41cf212c74e1c0df9e2838e8ebcb
Terry <71273972+BigTel2020@users.noreply.github.com>
2020-10-27 20:17:15 +0000

    lets try this

--- script.js
+++ script.js
@@ -3,9 +3,27 @@ var next = document.getElementById("next");
 var thumbnail = document.getElementsByClassName("thumbnail");
 var hero = document.getElementById("hero");
 var backgroungImg = new Array(
-    "images/bg1.png",
-    "images/bg2.png",
-    "images/bg3.png",
-    "images/bg4.png",
-    "images/bg5.png",
+    "./Radiance_img/bg1.png",
+    "./Radiance_img/bg2.png",
+    "./Radiance_img/bg3.png",
+    "./Radiance_img/bg4.png",
+    "./Radiance_img/bg5.png",
+    
 );
+let i = 0;
+next.onclick = function(){
+    if(i < 4){
+    hero.style.backgroundImage = 'url("'+backgroungImg[i+1]+'")';
+    thumbnail[i+1].classList.add("active");
+    thumbnail[i].classList.remove("active");
+    i++;
+    }
+}
+prev.onclick = function(){
+    if(i > 0){
+    hero.style.backgroundImage = 'url("'+backgroungImg[i-1]+'")';
+    thumbnail[i-1].classList.add("active");
+    thumbnail[i].classList.remove("active");
+    i--;
+    }
+}
